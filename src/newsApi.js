1f825ef3ea90538d0d11450bd93f7e3c7d1df679
Marcus Hellberg <marcus@vaadin.com>
2019-04-18 09:32:15 -0400

    filter tech

--- src/newsApi.js
+++ src/newsApi.js
@@ -1,4 +1,5 @@
 const apiKey = '52e56bba9e6e457893b7b23700a34465';
-const topHeadlinesUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=' + apiKey;
 
 export { apiKey, topHeadlinesUrl };
